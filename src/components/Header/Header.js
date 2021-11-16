a08ea6d2e1df188d6d15d7acdb63488d49cd0345
azertyValentin <etu.valentin.menoret@gmail.com>
2021-11-16 15:30:33 +0100

    :lipstick: add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header-container">
+            <h1>Fil Rouge</h1>
+        </div>
+    )
+}
+
+export default Header;
