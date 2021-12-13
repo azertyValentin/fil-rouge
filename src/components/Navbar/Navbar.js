a2803b7ddbdd2ff3f6642af9e67625ac47ef8af6
azertyValentin <etu.valentin.menoret@gmail.com>
2021-12-13 21:49:26 +0100

    :sparkles: add navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import './Navbar.css'
 import config from '../../config/config'
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import Welcome from '../Welcome/Welcome';
 
 function Navbar() {
     const parsedConfig = JSON.parse(JSON.stringify(config));
@@ -7,14 +9,45 @@ function Navbar() {
     const spaces = parsedConfig.spaces
 
     return (
-        <div className="navbar-container">
-            <h4>{buildingName}</h4>
-            <ul>
-                {spaces.map((space, i) =>
-                    <li key={i}>{space.name}</li>
-                )}
-            </ul>
-        </div>
+        <BrowserRouter>
+            <div className="navbar-container">
+                <h4>{buildingName}</h4>
+                <div className="navbar">
+                    {
+                        spaces.map((space) => {
+                            return (
+                                <NavLink 
+                                    key={"nl"+space.id}
+                                    to={"/"+space.id}
+                                    activeStyle={{ fontWeight: 'bold' }}
+                                    style={{ color: 'inherit', textDecoration: 'inherit', fontWeight: 'normal'}}
+                                >
+                                    <p>{space.name}</p>
+                                </NavLink>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <Switch>
+                {
+                    spaces.map((space) => {
+                        return (
+                            <Route key={space.id} path={"/"+space.id}>
+                                <div className="main-container">
+                                    <h1>Salut</h1>
+                                </div>
+                            </Route>
+                        )
+                    })
+                }
+                <Route path="/">
+                    <div className="main-container">
+                        <Welcome></Welcome>
+                    </div>
+                </Route>
+            </Switch>
+        </BrowserRouter>
     )
 }
 
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import './Welcome.css'
+
+function Welcome() {
+    return (
+        <div>
+            <h2 className="welcome-title">Bienvenue sur le projet Fil Rouge</h2>
+            <p>Ce projet vous permet de gérer l'affectation de salles et de bureaux d'un bâtiment.</p>
+            <p>Vous pouvez modifier la structure du bâtiment (nombre de salles, nom des bâtiments ...) dans le fichier de config : <b>config.json</b>.</p>
+            <p>Pour commencer, sélectionner un bâtiment dans le menu de gauche et affecter votre première salle à un membre de votre équipe.</p>
+            <p>Amusez-vous bien :)</p>
+        </div>
+    )
+}
+
+export default Welcome;
